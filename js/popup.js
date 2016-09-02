65096ef6c1d9c26bfe185bc90f1ca4cfdff340eb
Marginall <mistmee@mail.ru>
2016-09-02 19:11:39 +0300

    js

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,44 @@
 var link = document.querySelector(".map-btn");
 var popup = document.querySelector(".popup-form");
-var name = document.querySelector(".popup-name");
 var close = popup.querySelector(".popup-form-close");
+var name = popup.querySelector(".popup-name");
+var mail = popup.querySelector(".popup-mail");
+var form = popup.querySelector("form");
+var storage = localStorage.getItem("name")
+var overlay = document.querySelector(".modal-overlay");
 
 link.addEventListener("click", function(event) {
 event.preventDefault();
 popup.classList.add("popup-form-show");
-name.focus();
+overlay.classList.add("modal-overlay-show");
+
+if (storage) {
+  name.value = storage;
+  mail.focus();
+} else {
+  name.focus();
+}
 });
 
 close.addEventListener("click", function(event) {
 event.preventDefault();
 popup.classList.remove("popup-form-show");
+overlay.classList.remove("modal-overlay-show");
+});
+
+form.addEventListener("submit", function(event) {
+  if (!name.value || !mail.value) {
+    event.preventDefault();
+    console.log("Нужно ввести логин и пароль");
+  } else {
+    localStorage.setItem("name", name.value);
+  }
+});
+
+  window.addEventListener("keydown", function(event) {
+    if (event.key.code ===27) {
+      if (popup.classList.contains("popup-form-show")) {
+        popup.classList.romove(popup-form-show);
+    }
+  }
 });
